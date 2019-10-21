a4b9f3bd24ed5228689436b4f464731627ea090a
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2019-10-21 18:27:21 -0300

    updating build script

--- build.sh
+++ build.sh
@@ -1,8 +1,77 @@
-git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller
+#!/bin/bash
+
+DIRECTORY="manager/asperathos-manager"
+
+if [ -d "$DIRECTORY" ]; then
+    echo "UEP"
+    DEFAULT_BRANCH="stable"
+    cd manager/asperathos-manager
+    CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+    cd ../..
+    FOLDER_EXIST="true"
+fi
+
+while getopts b:rlh option
+do
+    case "${option}"
+    in
+        b) BRANCH=${OPTARG} ;;
+        r) RESET="true" ;;
+        l) LOCAL="true" ;;
+        h) HELP="true" ;;
+    esac
+done
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -b BRANCH [-r] [-l] [-h]"
+   echo -e "\t-b BRANCH   Select Branch - The branch you want to clone from (the same branch will be used for all projects). If you use -b with a branch different from the current one the files will be reseted and the new branch will be downloaded"
+   echo -e "\t-r                  Reset - If you want to reset the current asperathos files"
+   echo -e "\t-l          Local Rebuild - If you want to rebuild using the local files. If you use this flag the other's will be ignored."
+   exit 1
+}
+
+
+if [ "$HELP" ]
+then
+    helpFunction
+fi
+
+if [ -z "$BRANCH"]
+then
+    BRANCH=$DEFAULT_BRANCH
+fi
+
+
+if [[ -z "$LOCAL"  &&  "$RESET" ]] || [[ -z "$LOCAL"  && "$CURRENT_BRANCH" != "$BRANCH" ]]
+then
+    if [[ "$FOLDER_EXIST" ]]
+    then
+        echo "Deleting existing files"
+        rm -rf controller/asperathos-controller/
+        rm -rf visualizer/asperathos-visualizer/
+        rm -rf manager/asperathos-manager/
+        rm -rf monitor/asperathos-monitor/
+    else 
+        echo "You tried to reset but there are no files"
+fi
+fi
+
+
+
+
+if [ -z "$LOCAL" ]
+then
+    echo "Cloning the branch ${BRANCH}"
+    git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller -b $BRANCH
+    git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor -b $BRANCH
+    git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager -b $BRANCH
+    git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer -b $BRANCH
+fi
+
+echo 'Building local images'
 docker build controller/ -t asperathos_controller
-git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor
 docker build monitor/ -t asperathos_monitor
-git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager
 docker build manager/ -t asperathos_manager
-git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer
 docker build visualizer/ -t asperathos_visualizer
