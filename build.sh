d2f20b36343278f7e468b21ef9d8643a8ab1031e
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2019-10-22 15:30:56 -0300

    in progress

--- build.sh
+++ build.sh
@@ -2,25 +2,26 @@
 
 DIRECTORY="manager/asperathos-manager"
 
+DEFAULT_BRANCH="stable"
+BRANCH=-1
 if [ -d "$DIRECTORY" ]; then
-    echo "UEP"
-    DEFAULT_BRANCH="stable"
     cd manager/asperathos-manager
     CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
     cd ../..
     FOLDER_EXIST="true"
 fi
 
-while getopts b:rlh option
+
+while getopts brlh option
 do
     case "${option}"
     in
-        b) BRANCH=${OPTARG} ;;
         r) RESET="true" ;;
         l) LOCAL="true" ;;
         h) HELP="true" ;;
     esac
 done
+shift $((OPTIND-1))
 
 helpFunction()
 {
@@ -31,14 +32,19 @@ helpFunction()
    echo -e "\t-l          Local Rebuild - If you want to rebuild using the local files. If you use this flag the other's will be ignored."
    exit 1
 }
-
+echo "$@"
+if [ $1 ] && [ $1 == '-b' ]
+then
+    echo "WOW"
+fi
 
 if [ "$HELP" ]
 then
     helpFunction
 fi
 
-if [ -z "$BRANCH"]
+
+if [ "$BRANCH" -eq -1 ]
 then
     BRANCH=$DEFAULT_BRANCH
 fi
@@ -64,14 +70,14 @@ fi
 if [ -z "$LOCAL" ]
 then
     echo "Cloning the branch ${BRANCH}"
-    git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller -b $BRANCH
-    git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor -b $BRANCH
-    git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager -b $BRANCH
-    git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer -b $BRANCH
+    # git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller -b $BRANCH
+    # git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor -b $BRANCH
+    # git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager -b $BRANCH
+    # git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer -b $BRANCH
 fi
 
 echo 'Building local images'
-docker build controller/ -t asperathos_controller
-docker build monitor/ -t asperathos_monitor
-docker build manager/ -t asperathos_manager
-docker build visualizer/ -t asperathos_visualizer
+# docker build controller/ -t asperathos_controller
+# docker build monitor/ -t asperathos_monitor
+# docker build manager/ -t asperathos_manager
+# docker build visualizer/ -t asperathos_visualizer
