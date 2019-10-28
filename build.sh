b4917c7ccf1cd47400d2f0b7b3e1681abb10728e
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2019-10-28 16:20:43 -0300

    adding default branch to stable

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller
+git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller -b stable
 docker build controller/ -t asperathos_controller
-git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor
+git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor -b stable
 docker build monitor/ -t asperathos_monitor
-git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager
+git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager -b stable
 docker build manager/ -t asperathos_manager
-git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer
+git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer -b stable
 docker build visualizer/ -t asperathos_visualizer
